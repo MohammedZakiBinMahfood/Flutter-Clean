939afcc35c985d5eca8d23e1c2091ddb13d9ba3b
Mohammad Zaki <129006270+MohammedZakiBinMahfood@users.noreply.github.com>
2026-06-19 19:36:38 +0300

    Add

---
+++ flutter_clean_all_linux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Searching for Flutter projects..."
+
+find / -type f -name "pubspec.yaml" 2>/dev/null |
+while read -r file
+do
+    projectPath=$(dirname "$file")
+
+    echo "----------------------------------------"
+    echo "Cleaning: $projectPath"
+    echo "----------------------------------------"
+
+    (
+        cd "$projectPath" || exit
+        flutter clean
+    )
+
+    echo "Done."
+    echo
+done
+
+echo "Finished."
+
+
+
+chmod +x flutter_clean_all_linux.sh
+./flutter_clean_all_linux.sh
